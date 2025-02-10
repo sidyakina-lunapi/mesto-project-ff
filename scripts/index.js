8b8660d523f024c9ed8fa202920505461ed09232
sidyakina-lunapi <agafoncevvsevolod@gmail.com>
2025-02-10 20:49:32 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
-// @todo: Функция создания карточки
+function renderCard({ name, link }) {
 
-// @todo: Функция удаления карточки
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
 
-// @todo: Вывести карточки на страницу
+  cardElement.querySelector(".card__title").textContent = name;
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.setAttribute("alt", name);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteCard);
+
+  return cardElement;
+}
+
+function deleteCard() {
+  const cards = this.parentElement;
+  cards.remove();
+}
+
+
+initialCards.forEach(renderCard, deleteCard);
+placesList.append(cardElement);
