543298c5b36d1393a4d1017ff8a282826b28dbb6
sidyakina-lunapi <agafoncevvsevolod@gmail.com>
2025-03-23 19:06:56 +0300

    Доработка

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
-//использую сдачу проекта с помощью арxива
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(".", 'dist'),
+        path: path.resolve(__dirname, 'dist'),
         publicPath: '',
     },
     mode: 'development',
+    devtool: 'source-map',
     module: {
         rules: [
             {
                 test: /\.css$/,
                 exclude: /node_modules/,
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                ],
             },
             {
                 test: /\.js$/,
@@ -29,9 +34,16 @@ module.exports = {
                 test: /\.(png|jpg|jpeg|gif|svg)$/i,
                 type: 'asset/resource',
                 generator: {
-                    filename: 'images/[name][ext]', // Сохраняем изображения в папку images
+                    filename: 'images/[name][ext]',
                 },
-            }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]',
+                },
+            },
         ],
     },
     plugins: [
@@ -43,15 +55,23 @@ module.exports = {
             patterns: [
                 { from: 'src/images', to: 'images' },
                 { from: 'src/pages', to: 'pages' },
-                { from: 'src/blocks', to: 'blocks' }
+                { from: 'src/blocks', to: 'blocks' },
             ],
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
     ],
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+    },
     devServer: {
         static: {
-            directory: path.join(".", 'dist'),
+            directory: path.join(__dirname, 'dist'),
         },
         open: true,
+        hot: true,
     },
-    mode: 'development',
 };
